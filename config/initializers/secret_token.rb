792ba375bd76618de484abd8b27c5abcfdff1d42
drewe <drew2carlson@aol.com>
2013-06-20 17:28:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6c7b6544f37f5b2b320b277512cec4044e00e98ea040d514f5de8d88cf385c2cdec94ccdde7081bad0fcca302c4dc33d9113ba86b744f8ba8aee0e02b54a8054'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe User do
+
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+end
